b50ee8896970a2fbd7171483060ebc5ce897b719
Tomer Basan <tomerbasan@Tomers-MacBook-Air.local>
2026-02-14 17:50:31 +0200

    Update design: colorful name banners and 4-column mobile grid

--- frontend/script.js
+++ frontend/script.js
@@ -304,6 +304,30 @@ function getImageUrl(imageIdentifier) {
 }
 
 /**
+ * Get a random vibrant color for dog name banner
+ */
+function getRandomColor() {
+    const colors = [
+        '#FF6B6B', // Red
+        '#4ECDC4', // Teal
+        '#45B7D1', // Blue
+        '#FFA07A', // Light Salmon
+        '#98D8C8', // Mint
+        '#F7DC6F', // Yellow
+        '#BB8FCE', // Purple
+        '#85C1E2', // Sky Blue
+        '#F8B88B', // Peach
+        '#ABEBC6', // Light Green
+        '#F06292', // Pink
+        '#FFB74D', // Orange
+        '#81C784', // Green
+        '#64B5F6', // Light Blue
+        '#BA68C8', // Violet
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+/**
  * Display results
  */
 function displayResults(dogs) {
@@ -353,10 +377,15 @@ function displayResults(dogs) {
             imageHtml = `<img src="${primaryImage}" alt="${escapeHtml(dog.name)}" loading="lazy" data-index="0">`;
         }
 
+        // Get random color for name banner
+        const bannerColor = getRandomColor();
+
         card.innerHTML = `
+            <div class="dog-name-banner" style="background-color: ${bannerColor}">
+                ${escapeHtml(dog.name)}
+            </div>
             ${imageHtml}
             <div class="dog-card-info">
-                <div class="dog-card-name">${escapeHtml(dog.name)}</div>
                 <div class="dog-card-date">${date}</div>
                 ${detailsHtml}
             </div>
